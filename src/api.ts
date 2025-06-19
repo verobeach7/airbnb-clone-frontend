8130096b84cc4c4b7bf34c2a6757ad539d08baa2
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-06-19 18:00:51 +0900

    AirBnb Clone Frontend: React Query(Axios: data fetch)

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,16 @@
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+import axios from "axios";
 
-export async function getRooms() {
-  const response = await fetch(`${BASE_URL}/rooms/`);
-  const json = await response.json();
-  return json;
-}
+// Axios의 instance 생성 기능을 이용해 편리하게 이용할 수 있으며 오타로 인한 에러 발생 가능성도 줄여줌
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+});
+
+// export async function getRooms() {
+//   // axios가 fetch부터 await json까지 내부적으로 다 처리함
+//   const response = await instance.get(`rooms/`);
+//   return response.data;
+// }
+
+// 한 줄로 만들어 줄 수 있음
+export const getRooms = () =>
+  instance.get("rooms/").then((response) => response.data);
