431d65e1614cfe62f9f28491a062b9902187d7fc
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-06-21 19:31:15 +0900

    AirBnb Clone Frontend: React Query(Photos Grid)

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -2,6 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getRoom } from "../api";
 import type { IRoomDetail } from "../types";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Skeleton,
+} from "@chakra-ui/react";
 
 export default function RoomDetail() {
   const { roomPk } = useParams();
@@ -12,5 +20,33 @@ export default function RoomDetail() {
   });
   // ReactQueryDevtools 사용으로 더이상 Query로 받아온 데이터를 console.log()로 확인할 필요 없음.
   // console.log(data);
-  return <h1>hello</h1>;
+  return (
+    <Box mt={6} px={10}>
+      <Skeleton height={"30px"} width={"25%"} loading={isLoading}>
+        <Heading>{data?.name}</Heading>
+      </Skeleton>
+      <Grid
+        mt={6}
+        gap={3}
+        rounded={"2xl"}
+        // overflow={"hidden"} 없이는 rounded가 적용되지 않은 것처럼 보임
+        overflow={"hidden"}
+        height="50vh"
+        templateRows={"1fr 1fr"}
+        templateColumns={"repeat(4, 1fr)"}
+      >
+        {data?.photos.slice(0, 5).map((photo, index) => (
+          // overflow="hidden": Box 크기에서 넘치는 부분을 숨겨줌
+          <GridItem
+            colSpan={index === 0 ? 2 : 1}
+            rowSpan={index === 0 ? 2 : 1}
+            overflow={"hidden"}
+            key={photo.pk}
+          >
+            <Image objectFit={"cover"} w={"100%"} h={"100%"} src={photo.file} />
+          </GridItem>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
