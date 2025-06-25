9a710b4636bd51ce21f154869164825813ac8c13
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-06-25 21:47:09 +0900

    AirBnb Clone Frontend: Authentication Frontend with backend(Github OAuth Complete: useQueryClient, useNavigate, Toast, Eliminated Strict Mode for preventing do it twice)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
@@ -9,15 +8,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={client}>
-      {/* ChakraProvider를 이용해 테마와 구성을 모든 컴포넌트에 전달할 수 있음 */}
-      <ChakraProvider value={defaultSystem}>
-        <ColorModeProvider>
-          {/* Root router가 header, footer, child app을 렌더링 */}
-          <RouterProvider router={router} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={client}>
+    {/* ChakraProvider를 이용해 테마와 구성을 모든 컴포넌트에 전달할 수 있음 */}
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        {/* Root router가 header, footer, child app을 렌더링 */}
+        <RouterProvider router={router} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  </QueryClientProvider>
 );
--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,7 +1,9 @@
 import { Heading, Spinner, Text, VStack } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { githubLogIn } from "../api";
+import { toaster } from "../components/ui/toaster";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function NotFound() {
   // const location = useLocation();
@@ -14,6 +16,9 @@ export default function NotFound() {
 
   // location은 object로 그 안의 필요한 것을 꺼내어 사용
   const { search } = useLocation();
+  const queryClient = useQueryClient();
+  // useNavigate()는 user를 새로운 URL로 보내게 할 수 있음
+  const navigate = useNavigate();
   const confirmLogIn = async () => {
     // URLSearchParams: 브라우저 API로 String에서 필요한 것을 꺼내 사용
     const params = new URLSearchParams(search);
@@ -21,7 +26,22 @@ export default function NotFound() {
     // URL에서 얻은 code를 다시 장고(백엔드)로 보내줘야 함
     // code가 없을 수도 있으므로 있는지 체크한 후 있을 때만 실행해야 함
     if (code) {
-      await githubLogIn(code);
+      // React.JS는 Development Mode에서 두 번씩 작동하기에 githubLogIn()도 두 번 호출하게 됨 -> 백엔드에서 작은 오류를 발생시키게 됨
+      // 두 번씩 작동하는 것을 없애기 위해서 main.tsx의 StrictMode를 없앱버리면 됨
+      const status = await githubLogIn(code);
+      if (status == 200) {
+        toaster.create({
+          title: "Welcome!",
+          description: "Happy to have you back",
+          type: "success",
+        });
+        // 로그인에 성공한 후 아바타를 보여주기 위한 것
+        queryClient.refetchQueries({
+          queryKey: ["me"],
+        });
+        // 로그인 성공 시 이동할 URL
+        navigate("/");
+      }
     }
   };
   useEffect(() => {
