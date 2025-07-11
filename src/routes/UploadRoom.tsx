dd393ceff051b3e760241ff2e667649549b18d9e
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-07-11 15:40:17 +0900

    AirBnb Clone Frontend: Upload Room(Upload form: Chackra UI)

--- src/routes/UploadRoom.tsx
+++ src/routes/UploadRoom.tsx
@@ -1,5 +1,18 @@
+import {
+  Box,
+  Checkbox,
+  Container,
+  Field,
+  Heading,
+  Input,
+  InputGroup,
+  NativeSelect,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
 import useHostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
+import { FaBed, FaDollarSign, FaToilet } from "react-icons/fa";
 
 export default function UploadRoom() {
   // 두 가지 방식으로 사용할 수 있음
@@ -9,7 +22,101 @@ export default function UploadRoom() {
   useHostOnlyPage();
   return (
     <ProtectedPage>
-      <h1>upload roommmmmmm</h1>
+      <Box
+        pb={40}
+        mt={10}
+        px={{
+          base: 10,
+          lg: 40,
+        }}
+      >
+        <Container>
+          <Heading textAlign={"center"}>Upload Room</Heading>
+          <VStack gap={5} as="form" mt={5}>
+            <Field.Root>
+              <Field.Label>
+                Name
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <Input required type="text" />
+              <Field.HelperText>Write the name of your room.</Field.HelperText>
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>
+                Country
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <Input required type="text" />
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>
+                City
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <Input required type="text" />
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>
+                Address
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <Input required type="text" />
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>
+                Price
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <InputGroup startAddon={<FaDollarSign />}>
+                <Input required type="number" min={0} />
+              </InputGroup>
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>
+                Rooms
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <InputGroup startAddon={<FaBed />}>
+                <Input required type="number" min={0} />
+              </InputGroup>
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>
+                Toilets
+                <Field.RequiredIndicator />
+              </Field.Label>
+              <InputGroup startAddon={<FaToilet />}>
+                <Input required type="number" min={0} />
+              </InputGroup>
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>Description</Field.Label>
+              <Textarea />
+            </Field.Root>
+            <Field.Root>
+              <Checkbox.Root>
+                <Checkbox.HiddenInput />
+                <Checkbox.Control />
+                <Checkbox.Label>Pet friendly?</Checkbox.Label>
+              </Checkbox.Root>
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>Kind of room</Field.Label>
+              <NativeSelect.Root>
+                <NativeSelect.Field>
+                  <option value="entire_place">Entire Place</option>
+                  <option value="private_room">Private Room</option>
+                  <option value="shared_room">Shared Room</option>
+                </NativeSelect.Field>
+                <NativeSelect.Indicator />
+              </NativeSelect.Root>
+              <Field.HelperText>
+                What kind of room are you renting?
+              </Field.HelperText>
+            </Field.Root>
+          </VStack>
+        </Container>
+      </Box>{" "}
     </ProtectedPage>
   );
 }
